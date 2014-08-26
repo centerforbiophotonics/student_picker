8d49e584c991de3edfff93ff0cd924eba7ded8a1
matt-steinwachs <matt.steinwachs@gmail.com>
2014-08-26 14:03:40 -0700

    Removed duplicate application.js.

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,7 +2,7 @@ class Course < ActiveRecord::Base
   belongs_to :user
   has_many :courses_students, :foreign_key => "courses_id"
   has_many :students, :through => :courses_students
-  validates :name, presence: true
+  validates :name, :presence => true
   accepts_nested_attributes_for :user, :allow_destroy => true
 
   def to_csv
